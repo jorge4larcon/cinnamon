ac990291d324c8d987dce973d327611d7fb3e83e
jorge4larcon <jorge4larcon@gmail.com>
2019-10-11 09:46:40 -0500

    more logging messages

--- src/requests.rs
+++ src/requests.rs
@@ -4,6 +4,7 @@ extern crate log;
 use std::net;
 use std::fmt;
 use crate::ipparser;
+use crate::clients;
 use std::convert::TryFrom;
 
 
@@ -25,7 +26,10 @@ impl Request {
                         if let Some(usr) = usr.as_str() {
                             match usr.to_lowercase().as_str() {
                                 "admin" | "client" => user = String::from(usr.to_lowercase()),
-                                _ => return None
+                                _ => {
+                                    log::info!("Request::from - incorrect user type ({})", usr);
+                                    return None;
+                                }
                             }
                             log::debug!("Request::from - user obtained ({})", user);
                             if let Some(method) = request.get("method") {
@@ -42,11 +46,11 @@ impl Request {
                                                     if let Some(how) = how.as_str() {
                                                         match how.to_lowercase().as_str() {
                                                             "mac" => { // ClientRequest::GetByMac
-                                                                log::debug!("Request::from - parsing request: ClientRequest::Get how obtained (mac)");
+                                                                log::debug!("Request::from - parsing request: ClientRequest::GetByMac how obtained (mac)");
                                                                 if let Some(mac) = request.get("mac") {
                                                                     if let Some(mac) = mac.as_str() {
                                                                         if let Some(mac) = ipparser::MacAddress::new_from_str(mac) {
-                                                                            log::debug!("Request::from - parsing request: ClientRequest::Get mac obtained ({})", mac);
+                                                                            log::debug!("Request::from - parsing request: ClientRequest::GetByMac mac obtained ({})", mac);
                                                                             log::debug!("Request::from - parsed request: ClientRequest::GetByMac");
                                                                             return Some(Request::Client(ClientRequest::GetByMac { password, mac} ));
                                                                         }
@@ -54,26 +58,32 @@ impl Request {
                                                                 }
                                                             },
                                                             "username" => { // ClientRequest::GetByUsername
-                                                                log::debug!("Request::from - parsing request: ClientRequest::Get how obtained (username)");
+                                                                log::debug!("Request::from - parsing request: ClientRequest::GetByUsername how obtained (username)");
                                                                 if let Some(username) = request.get("username") {
                                                                     if let Some(username) = username.as_str() {
-                                                                        log::debug!("Request::from - parsing request: ClientRequest::Get username obtained ({})", username);
-                                                                        if let Some(start_index) = request.get("start_index") {
-                                                                            if let Some(start_index) = start_index.as_u64() {
-                                                                                let start_index = start_index as usize;
-                                                                                log::debug!("Request::from - parsing request: ClientRequest::Get start_index obtained ({})", start_index);
-                                                                                let username = String::from(username);
-                                                                                log::debug!("Request::from - parsed request: ClientRequest::GetByUsername");
-                                                                                return Some(Request::Client(ClientRequest::GetByUsername { password, username, start_index } ))
-                                                                            }
-                                                                        }
-                                                                    }
-                                                                }
+                                                                        if clients::Client::is_valid_username(username) {
+                                                                            log::debug!("Request::from - parsing request: ClientRequest::GetByUsername username obtained ({})", username);
+                                                                            if let Some(start_index) = request.get("start_index") {
+                                                                                if let Some(start_index) = start_index.as_u64() {
+                                                                                    if let Ok(start_index) = usize::try_from(start_index) {
+                                                                                        log::debug!("Request::from - parsing request: ClientRequest::GetByUsername start_index obtained ({})", start_index);
+                                                                                        let username = String::from(username);
+                                                                                        log::debug!("Request::from - parsed request: ClientRequest::GetByUsername");
+                                                                                        return Some(Request::Client(ClientRequest::GetByUsername { password, username, start_index } ))
+                                                                                    } else { log::debug!("Request::from - parsing request: ClientRequest::GetByUsername incorrect start_index ({})", start_index); }
+                                                                                } else { log::debug!("Request::from - parsing request: ClientRequest::GetByUsername start_index not obtained"); }
+                                                                            } else { log::debug!("Request::from - parsing request: ClientRequest::GetByUsername start_index not obtained"); }
+                                                                        } else { log::debug!("Request::from - parsing request: ClientRequest::GetByUsername incorrect username ({})", username); }
+                                                                    } else { log::debug!("Request::from - parsing request: ClientRequest::GetByUsername username not obtained"); }
+                                                                } else { log::debug!("Request::from - parsing request: ClientRequest::GetByUsername username not obtained"); }
                                                             },
-                                                            _ => return None
-                                                        }
-                                                    }
-                                                }
+                                                            _ => {
+                                                                log::debug!("Request::from - parsing request: ClientRequest::Get incorrect how type ({})", how);
+                                                                return None;
+                                                            }
+                                                        } 
+                                                    } else { log::debug!("Request::from - parsing request: ClientRequest::Get how not obtained"); }
+                                                } else { log::debug!("Request::from - parsing request: ClientRequest::Get how not obtained"); }
                                             }
                                         },
                                         "drop" => {
@@ -88,9 +98,9 @@ impl Request {
                                                             log::debug!("Request::from - parsing request: ClientRequest::Drop ip obtained ({})", ip);
                                                             log::debug!("Request::from - parsed request: ClientRequest::Drop");
                                                             return Some(Request::Client(ClientRequest::Drop { password, ip } ))
-                                                        }
-                                                    }
-                                                }
+                                                        } else { log::debug!("Request::from - parsing request: ClientRequest::Drop incorrect ip ({})", ip); }
+                                                    } else { log::debug!("Request::from - parsing request: ClientRequest::Drop ip not obtained"); }
+                                                } else { log::debug!("Request::from - parsing request: ClientRequest::Drop ip not obtained"); }
                                             }
                                         },
                                         "set" => { // Admin only
@@ -103,45 +113,47 @@ impl Request {
                                         log::debug!("Request::from - method obtained (sign_up)");
                                             if user != "client" { return None; }
                                             if let Some(username) = request.get("username") {
-                                                if let Some(username) = username.as_str() {                                                    
-                                                    let username = String::from(username);
-                                                    log::debug!("Request::from - parsing request: ClientRequest::SignUp username obtained ({})", username);
-                                                    if let Some(mac) = request.get("mac") {
-                                                        if let Some(mac) = mac.as_str() {
-                                                            if let Some(mac) = ipparser::MacAddress::new_from_str(mac) {
-                                                                log::debug!("Request::from - parsing request: ClientRequest::SignUp mac obtained ({})", mac);
-                                                                if let Some(port) = request.get("port") {
-                                                                    if let Some(port) = port.as_u64() {
-                                                                        if let Ok(port) = u16::try_from(port) {
-                                                                            log::debug!("Request::from - parsing request: ClientRequest::SignUp port obtained ({})", port);
-                                                                            if let Some(gobm) = request.get("get_only_by_mac") {
-                                                                                if let Some(get_only_by_mac) = gobm.as_bool() {
-                                                                                    log::debug!("Request::from - parsing request: ClientRequest::SignUp get_only_by_mac obtained ({})", get_only_by_mac);
-                                                                                    log::debug!("Request::from - parsed request: ClientRequest::SignUp");
-                                                                                    return Some(Request::Client(ClientRequest::SignUp { password, username, mac, port, get_only_by_mac } ));
-                                                                                }
-                                                                            }
-                                                                        }
-                                                                    }
-                                                                }
-                                                            }
-                                                        }
-                                                    }
-                                                }
-                                            }
+                                                if let Some(username) = username.as_str() {
+                                                    if clients::Client::is_valid_username(username) {
+                                                        let username = String::from(username);
+                                                        log::debug!("Request::from - parsing request: ClientRequest::SignUp username obtained ({})", username);
+                                                        if let Some(mac) = request.get("mac") {
+                                                            if let Some(mac) = mac.as_str() {
+                                                                if let Some(mac) = ipparser::MacAddress::new_from_str(mac) {
+                                                                    log::debug!("Request::from - parsing request: ClientRequest::SignUp mac obtained ({})", mac);
+                                                                    if let Some(port) = request.get("port") {
+                                                                        if let Some(port) = port.as_u64() {
+                                                                            if let Ok(port) = u16::try_from(port) {
+                                                                                log::debug!("Request::from - parsing request: ClientRequest::SignUp port obtained ({})", port);
+                                                                                if let Some(gobm) = request.get("get_only_by_mac") {
+                                                                                    if let Some(get_only_by_mac) = gobm.as_bool() {
+                                                                                        log::debug!("Request::from - parsing request: ClientRequest::SignUp get_only_by_mac obtained ({})", get_only_by_mac);
+                                                                                        log::debug!("Request::from - parsed request: ClientRequest::SignUp");
+                                                                                        return Some(Request::Client(ClientRequest::SignUp { password, username, mac, port, get_only_by_mac } ));
+                                                                                    } else { log::debug!("Request::from - parsing request: ClientRequest::SignUp get_only_by_mac not obtained"); }
+                                                                                } else { log::debug!("Request::from - parsing request: ClientRequest::SignUp get_only_by_mac not obtained"); }
+                                                                            } else { log::debug!("Request::from - parsing request: ClientRequest::SignUp incorrect port ({})", port); }
+                                                                        } else { log::debug!("Request::from - parsing request: ClientRequest::SignUp port not obtained"); }
+                                                                    } else { log::debug!("Request::from - parsing request: ClientRequest::SignUp port not obtained"); }
+                                                                } else { log::debug!("Request::from - parsing request: ClientRequest::SignUp incorrect mac ({})", mac); }
+                                                            } else { log::debug!("Request::from - parsing request: ClientRequest::SignUp username not obtained"); }
+                                                        } else { log::debug!("Request::from - parsing request: ClientRequest::SignUp mac not obtained"); }
+                                                    } else { log::debug!("Request::from - parsing request: ClientRequest::SignUp invalid username ({})", username); }
+                                                } else { log::debug!("Request::from - parsing request: ClientRequest::SignUp username not obtained"); }
+                                            } else { log::debug!("Request::from - parsing request: ClientRequest::SignUp username not obtained"); }
                                         },
                                         _ => {
-                                            log::debug!("Request::from - method not obtained");
+                                            log::debug!("Request::from - incorrect method type ({})", method);
                                             return None;
                                         }
                                     }
-                                }
-                            }
-                        }
-                    }
-                }
-            }            
-        }
+                                } else { log::debug!("Request::from - method not obtained"); }
+                            } else { log::debug!("Request::from - method not obtained"); }
+                        } else { log::debug!("Request::from - user not obtained"); }
+                    } else { log::debug!("Request::from - user not obtained"); }
+                } else { log::debug!("Request::from - password not obtained"); }
+            } else { log::debug!("Request::from - password not obtained"); }
+        } else { log::debug!("Request::from - failed to serde_json::from_str"); }        
         None
     }
 }
